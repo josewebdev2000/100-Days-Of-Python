afa9fd547ce19cd92d8d8627c4fdb19303453222
josewebdev2000 <josewebdev@proton.me>
2023-04-07 20:22:48 -0400

    Added Day-1 Challenge Solution

---
+++ day-1/main.py
@@ -0,0 +1,11 @@
+# Create a greeting for your program
+print("Welcome to the Brand Generator Program")
+
+# Ask the user for the city that they grew up in
+city = input("Enter the city you grew up in: ")
+
+# Ask the user for the name of a pet
+pet_name = input("Enter a name for a pet: ")
+
+# Combine the name of their city and pet and show them their brand name
+print("Your brand name is: " + city + " " + pet_name)
